8dcb765bf55e25ccdea6dae3406343b554f91968
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-09-27 10:15:07 -0700

    challenge 13

--- src/Inventory.js
+++ src/Inventory.js
@@ -4,10 +4,15 @@ const total = data.reduce( (acc, obj) => {
     return acc + parseFloat( obj.price.slice(1) )
 }, 0).toFixed(2);
 
+const totalUnits = data.reduce( (acc, obj) => {
+    return acc + obj.units
+}, 0 );
+
 function Inventory() {
     return (
         <div className="Inventory">
             <h3>Total cost of inventory: ${total}</h3>
+            <h3>Total Units: {totalUnits}</h3>
         </div>
     )
 }
